95237cc4f22b78c1d9b62e9658583883fc8c0e9e
Bill Nguyen Gia <nguyengiaphu0304@gmail.com>
2026-08-18 19:51:28 +0700

    feat: expose ratio policy API

--- src/statement_lens/__init__.py
+++ src/statement_lens/__init__.py
@@ -2,13 +2,16 @@
 
 from statement_lens.history import compare_documents, compare_history, compare_json
 from statement_lens.normalizer import ValidationError, normalize_document, normalize_json
+from statement_lens.ratios import evaluate_ratio_json, evaluate_ratio_policy
 
 __all__ = [
     "ValidationError",
     "compare_documents",
     "compare_history",
     "compare_json",
+    "evaluate_ratio_json",
+    "evaluate_ratio_policy",
     "normalize_document",
     "normalize_json",
 ]
-__version__ = "0.2.0"
+__version__ = "0.3.0a1"
